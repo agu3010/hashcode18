0ded332b820df1a940ccad1da805000f9ec1ff77
Raghav Arora <agu94.raghav@gmail.com>
2018-03-01 19:48:49 +0100

    Parsed Cars

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Ride:
-    def __init__(self, a, b, x, y, s, f):
+    def __init__(self, n, a, b, x, y, s, f):
+        self.rideNumber = n
         self.startPoint = [int(a),int(b)]
         self.finishPoint = [int(x),int(y)]
         self.minStart = int(s)
@@ -9,8 +10,12 @@ class Ride:
         self.possible = self.isPossible()
 
     def __repr__(self):
-        return "Start: ({},{}) on or after {} | End: ({},{}) on or before {} | Distance: {} | Possible : {}".\
-            format(self.startPoint[0], self.startPoint[1], self.minStart, self.finishPoint[0], self.finishPoint[1], self.maxFinish, self.distance, self.possible)
+        return """Ride no. {}
+        Start: ({},{}) on or after {} | End: ({},{}) on or before {}
+        Distance: {} | Possible : {}""".\
+            format(self.rideNumber, self.startPoint[0], self.startPoint[1],
+            self.minStart, self.finishPoint[0], self.finishPoint[1],
+            self.maxFinish, self.distance, self.possible)
 
     def isPossible(self):
         if self.maxFinish - self.minStart > self.distance:
@@ -21,6 +26,20 @@ class Ride:
         self.completed = True
 
 
+
+class Car:
+    def __init__(self):
+        self.location = [0,0]
+        self.ridesDone = []
+        self.currentRide = 0
+        self.assignedRide = 0
+
+    def __repr__(self):
+        if self.currentRide:
+            return "Car currently on Ride <Ride no. {}>".format(self.currentRide)
+        elif self.assignedRide:
+            return "Car assigned to Ride <Ride no. {}>".format(self.assignedRide)
+        return "Car available at {}".format(self.location)
 
 
 lines = [line.rstrip('\n') for line in open('a_example.in')]
@@ -35,9 +54,12 @@ B = int(info[4])
 T = int(info[5])
 
 
-print N
+
+for i in range(F):
+    car = Car()
+    print car
 
 for i in range(N):
     rideInfo = lines[i].split()
-    ride = Ride(rideInfo[0], rideInfo[1], rideInfo[2], rideInfo[3], rideInfo[4], rideInfo[5])
+    ride = Ride(i+1, rideInfo[0], rideInfo[1], rideInfo[2], rideInfo[3], rideInfo[4], rideInfo[5])
     print ride
